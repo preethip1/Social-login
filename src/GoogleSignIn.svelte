e4c6374865680d916d1f0f7c0d42f169e581ece6
preethip1 <preethiknot1998@gmail.com>
2020-11-10 12:41:02 +0530

    Login using github

---
+++ src/GoogleSignIn.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { GoogleAuth } from "@beyonk/svelte-social-auth";
+  let name;
+  let login = false;
+  function onSignIn(e) {
+    login = true;
+    let user = e.detail.user;
+    let profile = user.getBasicProfile();
+    name = profile.getName();
+  }
+</script>
+
+<div class = "mt-2">
+  {#if login == false}
+    <GoogleAuth
+      clientId="Client_ID"
+      on:auth-success={onSignIn} />
+      {:else}
+      <div>{name} is logged in </div>
+  {/if}
+</div>
